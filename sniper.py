a33221ac2b7bbe3d1f4eb728a9384c4105a6fc2e
P-fury <plutocamera@gmail.com>
2024-03-25 16:06:08 +0100

    simple search

--- sniper.py
+++ sniper.py
@@ -1,16 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+import re
 
+with open('prices.txt', 'r') as file:
+    data = []
+    for line in file:
+        line = line.rstrip()
+        data.append(line)
 
+print(data)
 driver = webdriver.Chrome()
 
-url = "https://www.zalando.pl/adidas-originals-gazelle-bold-sneakersy-niskie-collegiate-red-lucid-pink-core-white-ad111a25a-g14.html"
-
-driver.get(url)
-price = driver.find_element(By.CSS_SELECTOR,"span.sDq_FX._4sa1cA.FxZV-M.HlZ_Tf")
-
-print(price.text)
-time.sleep(5)
-
-
+for i in range(0, len(data), 2):
+    link = data[i]
+    driver.get(link)
+    prices = driver.find_elements(By.XPATH, "//*[contains(., '\xa0zł')][following-sibling::span[contains(., 'w tym VAT')]]")
+    for price in prices:
+        clean_price = re.match("\d+,\d+", price.text)
+        if clean_price.group() <= data[i + 1]:
+            print(True)
+        else:
+            print(False)
